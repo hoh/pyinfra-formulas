ebf2c5c7c8592d2eb468a214c9e7fc48e4c351f5
Hugo Herter <git@hugoherter.com>
2016-10-01 13:18:13 +0200

    Add formula nginx.static_website

---
+++ formulas/nginx/__init__.py
@@ -0,0 +1,76 @@
+'''
+Formulas for serving websites or web apps via Nginx.
+'''
+
+from hereby import Here
+from pyinfra.modules import server, files, apt, init
+
+here = Here(__file__)
+
+
+def _deploy_user(name='deploy', group='deploy'):
+    '''
+    Configure a user to own files served by Nginx.
+
+    + name: username
+    + group: name of the group
+    '''
+    if not group:
+        server.user(
+            name,
+            ensure_home=True,
+        )
+    else:
+        server.group(
+            group,
+        )
+
+        server.user(
+            name,
+            group=group,
+            ensure_home=True,
+        )
+    return name
+
+
+def static_website(name, directory):
+    '''
+    Configure Nginx to serve the files in `directory`.
+
+    + name: domain name used to serve the files
+    + directory: path to sync files from
+    '''
+    apt.packages(
+        ['nginx'],
+    )
+
+    user, group = _deploy_user()
+
+    files.directory(
+        '/srv/web',
+        user=user,
+        group=group,
+    )
+
+    files.directory(
+        '/srv/web/{}/'.format(name),
+        user=user,
+        group=group,
+    )
+    files.sync(
+        'files/web/{}/'.format(name),
+        '/srv/web/{}/'.format(name),
+        user=user,
+        group=group,
+        # delete=True,
+    )
+
+    if files.template(
+        here.abspath('files/static_website.nginx'),
+        '/etc/nginx/sites-enabled/{}'.format(name),
+        domain=name,
+    ).changed:
+        init.systemd(
+            'nginx',
+            reloaded=True,
+        )
